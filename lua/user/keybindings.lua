c65e57fbe28fc473863a625d6805bd7809989472
vuppalac <chandrashekarvuppala@gmail.com>
2022-04-26 10:32:29 +0530

    sync from abz

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -233,6 +233,10 @@ M.config = function()
     set_harpoon_keymaps()
   end
   lvim.keys.visual_mode["p"] = [["_dP]]
+  if lvim.builtin.refactoring.active then
+    lvim.keys.visual_mode["ga"] =
+      "<Esc><cmd>lua local ok, _ = require('refactoring') if ok then require('telescope').extensions.refactoring.refactors() else vim.lsp.buf.range_code_action() end<CR>"
+  end
   lvim.keys.visual_mode["<leader>st"] = "<Cmd>lua require('user.telescope').grep_string_visual()<CR>"
 
   -- WhichKey keybindings
@@ -268,7 +272,7 @@ M.config = function()
     z = { "<cmd>lua require('user.telescope').search_only_certain_files()<cr>", "Certain Filetype" },
   }
   lvim.builtin.which_key.mappings["C"] = { "<cmd>Telescope command_center<cr>", " Command Palette" }
-  -- lvim.keys.normal_mode["<c-P>"] = "<cmd>Telescope command_center<cr>"
+  lvim.keys.normal_mode["<c-P>"] = "<cmd>Telescope command_center<cr>"
 
   if lvim.builtin.file_browser.active then
     lvim.builtin.which_key.mappings["se"] = { "<cmd>Telescope file_browser<cr>", "File Browser" }
